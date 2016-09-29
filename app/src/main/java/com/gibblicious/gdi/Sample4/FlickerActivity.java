c0f0761941a26be13045be037ac49d30950313e6
Marcelle Gibble <marcelle.gibble@ticketmaster.com>
2016-09-28 23:46:43 -0400

    add dialogs

--- app/src/main/java/com/gibblicious/gdi/Sample4/FlickerActivity.java
+++ app/src/main/java/com/gibblicious/gdi/Sample4/FlickerActivity.java
@@ -2,6 +2,7 @@ package com.gibblicious.gdi.Sample4;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -66,6 +67,10 @@ public class FlickerActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onDialogClick(View v) {
+        startActivity(new Intent(this, ShowDialogsActivity.class));
     }
 
     private void setupViews() {
